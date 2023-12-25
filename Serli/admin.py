0402dfb176013ed42a7a7905080057600a631a00
NuruIzzah <nurulizzahreal@gmail.com>
2023-12-25 11:17:16 +0700

    lagi- lagi upload

--- Serli/admin.py
+++ Serli/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import KategoriMenu
+from .models import Menu
+from .models import Pesanan
+from .models import Pelanggan
+from .models import Pemesan
 # Register your models here.
+admin.site.register(KategoriMenu)
+admin.site.register(Menu)
+admin.site.register(Pesanan)
+admin.site.register(Pelanggan)
+admin.site.register(Pemesan)
