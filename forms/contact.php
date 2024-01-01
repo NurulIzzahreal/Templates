3134840510a7a6f5fdf69e8fb10f88dab326559f
NuruIzzah <nurulizzahreal@gmail.com>
2024-01-01 11:35:22 +0700

    upload

--- forms/contact.php
+++ forms/contact.php
@@ -7,34 +7,36 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+$receiving_email_address = 'contact@example.com';
 
-  if( file_exists($php_email_form = '{% static "vendor/php-email-form/php-email-form.php" %}') { include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+$php_email_form_path = 'vendor/php-email-form/php-email-form.php';  // Update this path accordingly
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+if (file_exists($php_email_form_path)) {
+    include($php_email_form_path);
+} else {
+    die('Unable to load the "PHP Email Form" Library!');
+}
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
+$contact = new PHP_Email_Form;
+$contact->ajax = true;
+
+$contact->to = $receiving_email_address;
+$contact->from_name = $_POST['name'];
+$contact->from_email = $_POST['email'];
+$contact->subject = $_POST['subject'];
+
+// Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+/*
+$contact->smtp = array(
     'host' => 'example.com',
     'username' => 'example',
     'password' => 'pass',
     'port' => '587'
-  );
-  */
+);
+*/
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+$contact->add_message($_POST['name'], 'From');
+$contact->add_message($_POST['email'], 'Email');
+$contact->add_message($_POST['message'], 'Message', 10);
 
-  echo $contact->send();
-?>
+echo $contact->send();
